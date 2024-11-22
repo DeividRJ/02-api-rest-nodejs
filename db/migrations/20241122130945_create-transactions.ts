dfda7a7a831e675804b22a999d30bb5a94e57b1b
Deivid Silva <deividsky2009@hotmail.com>
2024-11-22 11:41:50 -0300

    Banco de dados - Realizando queries com Knex

--- db/migrations/20241122130945_create-transactions.ts
+++ db/migrations/20241122130945_create-transactions.ts
@@ -1,5 +1,4 @@
-import type { Knex } from 'knex'
-
+import { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('transactions', (table) => {
     table.uuid('id').primary()
@@ -8,7 +7,6 @@ export async function up(knex: Knex): Promise<void> {
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
   })
 }
-
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTable('transactions')
 }
